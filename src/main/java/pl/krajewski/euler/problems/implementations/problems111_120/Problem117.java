894ac8f448bd620c8b81d67931e0911ef0032e55
Marcin Krajewski <mkrajewski.work@gmail.com>
2012-04-03 15:08:08 +0000

    Problem 117

--- src/main/java/pl/krajewski/euler/problems/implementations/problems111_120/Problem117.java
+++ src/main/java/pl/krajewski/euler/problems/implementations/problems111_120/Problem117.java
@@ -3,42 +3,33 @@ package pl.krajewski.euler.problems.implementations.problems111_120;
 import java.util.HashMap;
 import java.util.Map;
 
-import pl.krajewski.euler.ProblemGetter;
 import pl.krajewski.euler.problems.Parameters;
 import pl.krajewski.euler.problems.Problem;
-import pl.krajewski.euler.problems.utils.classes.MapDoubleKey;
 import pl.krajewski.euler.problems.utils.numbers.TilesUtil;
 
-public class Problem116 extends Problem<Long> {
+public class Problem117 extends Problem<Long> {
 
 	@Override
 	public Long getCorrectProblemAnswer() {
-		return 20492570929L;
+		return 100808458960497L;
 	}
 
 	@Override
 	protected Map<Parameters, Long> getTestAnswers() {
 		Map<Parameters, Long> answers = new HashMap<Parameters, Long>();
-		answers.put(new Parameters(5, 4), 12L);
+		answers.put(new Parameters(5), 15L);
 		return answers;
 	}
 
 	@Override
 	public Long resolveProblem() {
 		Integer maxTiles = getParameterForNumber(0);
-		Integer colorsNumber = getParameterForNumber(1);
-
-		long sum = 0;
-		for (int i = 2; i <= colorsNumber; i++) {
-			long moves = TilesUtil.getInstance().getMovesWithNotReturningColors(maxTiles, i);
-			sum += moves;
-		}
-		return sum;
+		return TilesUtil.getInstance().getMovesWithReturningColors(maxTiles);
 	}
 
 	@Override
 	protected Parameters getParametersForProblem() {
-		return new Parameters(50, 4);
+		return new Parameters(50);
 	}
 
 }
--- src/main/java/pl/krajewski/euler/problems/utils/numbers/TilesUtil.java
+++ src/main/java/pl/krajewski/euler/problems/utils/numbers/TilesUtil.java
@@ -16,28 +16,63 @@ public class TilesUtil {
 		return instance;
 	}
 
-	public long getMoves(int tiles, int len) {
+	public long getMovesWithNotReturningColors(int tiles, int len) {
 		if (tiles < len) {
 			return 0L;
 		}
 		if (tiles == len) {
 			return 1L;
 		}
-		return getMovesWithCacheMap(tiles, len,
-				new MapDoubleKey<Integer, Integer, Long>());
-	}
 
-	public long getMovesWithCacheMap(int tiles, int len,
-			MapDoubleKey<Integer, Integer, Long> cacheMap) {
 		Long sum = 0L;
 		for (int singleNumber = 0; singleNumber < tiles; singleNumber++) {
 			for (int colorNumber = 1; colorNumber <= tiles; colorNumber++) {
 				if (tiles == (singleNumber + colorNumber * len)) {
-					sum += TilesUtil.getInstance().moves(colorNumber - 1,
-							singleNumber, cacheMap);
-					if (singleNumber > 0) {
-						sum += TilesUtil.getInstance().moves(colorNumber,
-								singleNumber - 1, cacheMap);
+					sum += nonReturnMoves(colorNumber, singleNumber,
+							new MapDoubleKey<Integer, Integer, Long>());
+				}
+			}
+		}
+		return sum;
+	}
+
+	private Long nonReturnMoves(int colors, int single,
+			MapDoubleKey<Integer, Integer, Long> map) {
+
+		if (colors == 0 && single == 0) {
+			return 1L;
+		}
+
+		if (map.get(colors, single) != null) {
+			return map.get(colors, single);
+		}
+
+		long moves1 = 0;
+		if (colors > 0) {
+			moves1 = nonReturnMoves(colors - 1, single, map);
+			map.put(colors - 1, single, moves1);
+		}
+		long moves2 = 0;
+		if (single > 0) {
+			moves2 = nonReturnMoves(colors, single - 1, map);
+			map.put(colors, single - 1, moves2);
+		}
+		return moves1 + moves2;
+	}
+
+	public long getMovesWithReturningColors(int tiles) {
+
+		Long sum = 0L;
+
+		Long[][][][] tab = new Long[tiles + 1][tiles + 1][tiles + 1][tiles + 1];
+
+		for (int green = 0; green < tiles; green++) {
+			for (int red = 0; red < tiles; red++) {
+				for (int blue = 0; blue < tiles; blue++) {
+					for (int single = 0; single <= tiles; single++) {
+						if (tiles == (single + 2 * red + 3 * green + 4 * blue)) {
+							sum += returnMoves(red, green, blue, single, tab);
+						}
 					}
 				}
 			}
@@ -45,21 +80,40 @@ public class TilesUtil {
 		return sum;
 	}
 
-	private Long moves(int colors, int single,
-			MapDoubleKey<Integer, Integer, Long> map) {
-		
-		if (map.get(colors, single) != null) {
-			return map.get(colors, single);
-		}
-		
-		if (colors <= 0 || single <= 0) {
+	private Long returnMoves(int red, int green, int blue, int single,
+			Long[][][][] tab) {
+
+		if (red == 0 && green == 0 && blue == 0 && single == 0) {
 			return 1L;
 		}
-		
-		long moves1 = moves(colors - 1, single, map);
-		long moves2 = moves(colors, single - 1, map);
-		map.put(colors - 1, single, moves1);
-		map.put(colors, single - 1, moves2);
-		return moves1 + moves2;
+
+		if (tab[red][green][blue][single] != null) {
+			return tab[red][green][blue][single];
+		}
+
+		long sum = 0;
+		long moves;
+
+		if (red > 0) {
+			moves = returnMoves(red - 1, green, blue, single, tab);
+			tab[red - 1][green][blue][single] = moves;
+			sum += moves;
+		}
+		if (green > 0) {
+			moves = returnMoves(red, green - 1, blue, single, tab);
+			tab[red][green - 1][blue][single] = moves;
+			sum += moves;
+		}
+		if (blue > 0) {
+			moves = returnMoves(red, green, blue - 1, single, tab);
+			tab[red][green][blue - 1][single] = moves;
+			sum += moves;
+		}
+		if (single > 0) {
+			moves = returnMoves(red, green, blue, single - 1, tab);
+			tab[red][green][blue][single - 1] = moves;
+			sum += moves;
+		}
+		return sum;
 	}
 }
