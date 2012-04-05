1bde340298439de1c3b2f758bb95d4ee8bfa0305
Marcin Krajewski <mkrajewski.work@gmail.com>
2012-04-05 09:28:31 +0000

    refactor

--- src/main/java/pl/krajewski/euler/problems/implementations/problems051_060/Problem052.java
+++ src/main/java/pl/krajewski/euler/problems/implementations/problems051_060/Problem052.java
@@ -17,56 +17,52 @@ public class Problem052 extends Problem<Long> {
 	@Override
 	public Long resolveProblem() {
 		
-		long[] mul = new long[5];
+		long[] multiplyArray = new long[5];
 
 		for (long i = 2;; i++) {
-			mul[0] = i * 2;
-			if (hasDoubleDigits(mul[0])) {
+			multiplyArray[0] = i * 2;
+			if (hasDoubleDigits(multiplyArray[0])) {
 				continue;
 			}
-			mul[1] = i * 3;
-			mul[2] = i * 4;
-			mul[3] = i * 5;
-			mul[4] = i * 6;
-			for (int ind = 0; ind < 4; ind++) {
-				if (!eq(mul[ind], mul[ind + 1])) {
+			multiplyArray[1] = i * 3;
+			multiplyArray[2] = i * 4;
+			multiplyArray[3] = i * 5;
+			multiplyArray[4] = i * 6;
+			for (int index = 0; index < 4; index++) {
+				if (!numbersContainsTheSameDifferentDigits(multiplyArray[index], multiplyArray[index + 1])) {
 					break;
 				}
-				if(ind == 3) {
+				if(index == 3) {
 					return i;
 				}
 			}
 		}
 	}
 
-	private boolean eq(long mul2, long mul3) {
-		int[] a = StringAsNum.getStringAsNumArray0IsLowerMaxIsHigher(mul2 + "");
-		Set<Integer> set = new HashSet<Integer>();
-		for (int num : a) {
-			set.add(num);
+	private boolean numbersContainsTheSameDifferentDigits(long number1, long number2) {
+		int[] number1Digits = StringAsNum.getStringAsNumArray0IsLowerMaxIsHigher(number1 + "");
+		Set<Integer> number1DigitsSet = new HashSet<Integer>();
+		for (int digit : number1Digits) {
+			number1DigitsSet.add(digit);
 		}
-		int[] b = StringAsNum.getStringAsNumArray0IsLowerMaxIsHigher(mul3 + "");
-		for (int num : b) {
-			if (!set.contains(num)) {
+		int[] number2Digits = StringAsNum.getStringAsNumArray0IsLowerMaxIsHigher(number2 + "");
+		for (int digit : number2Digits) {
+			if (!number1DigitsSet.contains(digit)) {
 				return false;
 			}
 		}
 		return true;
 	}
 
-	private boolean hasDoubleDigits(long len2) {
-		int[] a = StringAsNum.getStringAsNumArray0IsLowerMaxIsHigher(len2 + "");
-		Set<Integer> set = new HashSet<Integer>();
-		for (int num : a) {
-			if (!set.add(num)) {
+	private boolean hasDoubleDigits(long number) {
+		int[] numberDigits = StringAsNum.getStringAsNumArray0IsLowerMaxIsHigher(number + "");
+		Set<Integer> numberDigitsSet = new HashSet<Integer>();
+		for (int digit : numberDigits) {
+			if (!numberDigitsSet.add(digit)) {
 				return true;
 			}
 		}
 		return false;
-	}
-
-	private int getLen(long value) {
-		return String.valueOf(value).length();
 	}
 
 	@Override
