5d6941a8cc897afb6bfa254501e5fef59a3d328a
Marcin Krajewski <mkrajewski.work@gmail.com>
2012-04-09 17:28:02 +0000

    problem 53

---
+++ src/main/java/pl/krajewski/euler/problems/implementations/problems051_060/Problem053.java
@@ -0,0 +1,36 @@
+package pl.krajewski.euler.problems.implementations.problems051_060;
+
+import pl.krajewski.euler.problems.Parameters;
+import pl.krajewski.euler.problems.Problem;
+import pl.krajewski.euler.problems.utils.numbers.BinomialCoefficient;
+
+public class Problem053 extends Problem<Integer> {
+
+	@Override
+	public Integer getCorrectProblemAnswer() {
+		return 4075;
+	}
+
+	@Override
+	public Integer resolveProblem() {
+		Integer max = getParameterForNumber(0);
+		Integer min = getParameterForNumber(1);
+
+		int result = 0;
+		for (int n = min; n <= max; n++) {
+			for (int k = 1; k <= n; k++) {
+				if (BinomialCoefficient.isBinomialCoefficientGreaterThan(n, k,
+						1000000)) {
+					result++;
+				}
+			}
+		}
+		return result;
+	}
+
+	@Override
+	protected Parameters getParametersForProblem() {
+		return new Parameters(100, 23);
+	}
+
+}
